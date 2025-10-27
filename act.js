e34ac78c38bab804f7615baddd1c3ab85160bedb
adobong-sunog <craftydaman7@gmail.com>
2025-10-27 20:51:30 +0800

    multiply commit

--- act.js
+++ act.js
@@ -6,8 +6,13 @@ function subtract(a, b) {
     return a - b;
 }
 
+function multiply(a, b) {
+    return a * b;
+}
+
 let x = 7;
 let y = 3;
 
 console.log("Add : " + add(x, y));
 console.log("Square: " + square(x, y));
+console.log("Multiply: " + multiply(x, y));
