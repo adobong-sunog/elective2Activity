0a2d622bbbe4f1bb51f531ffafd5b5b9999a6c1a
asadosauce <craftydaman7@gmail.com>
2025-10-27 20:52:53 +0800

    add square function in square branch

--- act.js
+++ act.js
@@ -1,23 +1,3 @@
-function add(x, y) {
-  return x + y;
+function square(x, y) {
+    return "Square of x :" + (x * x) + ", Square of y :" + (y * y);
 }
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-let x = 7;
-let y = 3;
-
-console.log("Add : " + add(x, y));
-console.log("Square: " + square(x, y));
-console.log("Multiply: " + multiply(x, y));
-console.log("Divide: " + divide(x, y));
