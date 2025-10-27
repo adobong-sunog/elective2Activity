e51196856aefd370b3e675f757bb187791052445
adobong-sunog <craftydaman7@gmail.com>
2025-10-27 20:51:59 +0800

    divide commit

--- act.js
+++ act.js
@@ -10,9 +10,14 @@ function multiply(a, b) {
     return a * b;
 }
 
+function divide(a, b) {
+    return a / b;
+}
+
 let x = 7;
 let y = 3;
 
 console.log("Add : " + add(x, y));
 console.log("Square: " + square(x, y));
 console.log("Multiply: " + multiply(x, y));
+console.log("Divide: " + divide(x, y));
